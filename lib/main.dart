74d1ba5e0f6b7a331b22e977d6a6a68a1abd7844
frankmungnodev <jeremygornlay04@gmail.com>
2022-10-20 09:43:15 +0530

    Added drift dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/screen_app.dart';
+import 'package:todo_list/screens/screen_app.dart';
 
 void main() {
   runApp(const App());
