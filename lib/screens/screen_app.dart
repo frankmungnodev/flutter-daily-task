9db46201085f31610e2de4ed978393eb03732d95
frankmungnodev <jeremygornlay04@gmail.com>
2022-10-20 09:11:08 +0530

    Added gitignore file

---
+++ lib/screens/screen_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenApp extends StatefulWidget {
+  const ScreenApp({Key? key}) : super(key: key);
+
+  @override
+  ScreenAppState createState() => ScreenAppState();
+}
+
+class ScreenAppState extends State<ScreenApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
