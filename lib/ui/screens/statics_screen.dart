76ef21db5142274972dac89a4b345d2f62087dee
frankmungnodev <jeremygornlay04@gmail.com>
2022-10-28 17:18:41 +0530

    Init statistic screen

--- lib/ui/screens/statics_screen.dart
+++ lib/ui/screens/statics_screen.dart
@@ -5,6 +5,21 @@ class StaticsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center();
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: ListView(
+        clipBehavior: Clip.none,
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              'Statistics',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
